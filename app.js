f8ca1fd608780216e2ddc5bc79421ef406f8a666
Skelton <mskelton1@una.edu>
2023-01-19 16:38:06 -0600

    updated version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World This is Meghan!')
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
 
 console.log('in the node console');
 
-app.listen(3000, () => {
-    console.log(`Server is running & listening on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running & listening on port ${ PORT }`)
   });
